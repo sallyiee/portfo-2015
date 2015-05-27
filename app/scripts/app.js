ac0a95a67f4d434541baa30ebe30bd322ae0ccb0
sallyiee <amy.elly.lee@gmail.com>
2015-05-27 20:54:36 +1000

    Portfolio-2015
    My personal portfolio

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfo2015App
+ * @description
+ * # portfo2015App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfo2015App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfo2015App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the portfo2015App
+ */
+angular.module('portfo2015App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
